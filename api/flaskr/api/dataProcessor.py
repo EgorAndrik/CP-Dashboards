a791dde6bffd7741f08df3a1fa83649c9211efb3
Egor Andreasyan <124351915+EgorAndrik@users.noreply.github.com>
2024-06-01 20:45:46 +0700

    Update dataProcessor.py

--- api/flaskr/api/dataProcessor.py
+++ api/flaskr/api/dataProcessor.py
@@ -186,7 +186,7 @@ class DataPreprocessing:
 
         rate_subpolygons['result_score'] = rate_subpolygons['mileage_deviation_score'] * 0.4 + rate_subpolygons['subpolygon_cars_score'] * 0.3 + rate_subpolygons['penalty_score'] * 0.15 + rate_subpolygons['driving_style_score'] * 0.15
 
-        rate_subpolygons
+        return rate_subpolygons
 
 
 
